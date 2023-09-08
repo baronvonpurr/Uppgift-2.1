03e9f3970ad516226d67c340f8965650041602af
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-08 13:28:59 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken stad bor du i?");
+            string plats = Console.ReadLine();
+            Console.WriteLine("Vilket är ditt drömresemål?");
+            string resemål = Console.ReadLine();
+            Console.WriteLine("Hej " +  plats + "-bo. Hoppas du får resa till " + resemål);
+        }
+    }
+}
